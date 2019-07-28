f8893739f2a51f73f37004704529f19ec1e9c2a2
Victor Leandro <samuellv010@hotmail.com>
2019-07-27 23:47:52 -0300

    init project

---
+++ src/resolvers.js
@@ -0,0 +1,15 @@
+const users = [
+    { id: 1 , name: "Teste 1", email: "teste1@teste.com" },
+    { id: 2 , name: "Teste 2", email: "teste2@teste.com" }
+]
+
+module.exports = {
+    Query: {
+        users: () => {users},
+        user: () => {users[0]}
+    },
+
+    Mutation: {
+        createUser: () => {users[0]}
+    }
+};
